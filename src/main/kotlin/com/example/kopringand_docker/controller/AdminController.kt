2e3293f1f8dc98d2a0290cb70d05cb97df510b8b
dokdogalmaegi <clik202012@gmail.com>
2023-02-28 22:14:07 +0900

    Complete Impleent deleteUser API

--- src/main/kotlin/com/example/kopringand_docker/controller/AdminController.kt
+++ src/main/kotlin/com/example/kopringand_docker/controller/AdminController.kt
@@ -5,17 +5,21 @@ import com.example.kopringand_docker.vo.FailResponseVO
 import com.example.kopringand_docker.vo.ResponseVO
 import com.example.kopringand_docker.vo.SuccessResponseVO
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import java.lang.Exception
+import java.util.Objects
 
 @RestController
 @RequestMapping("/admin")
 class AdminController(private val userInfoService: UserInfoService) {
 
     @PostMapping("/deleteUser")
-    fun deleteUser(@RequestParam("userId") userId: String): ResponseVO {
+    fun deleteUser(@RequestBody user: HashMap<String, Object>): ResponseVO {
+        val userId = user["userId"].toString();
+
         return try {
             SuccessResponseVO("Success delete $userId", userInfoService.deleteUser(userId))
         } catch (e: Exception) {
